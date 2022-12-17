930b9842cf7290ae3aa4eb8b33b186665fed49e5
zhenghaoz <zhangzhenghao@hotmail.com>
2022-12-17 20:45:05 +0800

    support insert feedback and get recommend

--- lib/gorse.rb
+++ lib/gorse.rb
@@ -1,7 +1,35 @@
-class User
+require 'json'
+require 'net/http'
+
+class Feedback
+  def initialize(feedback_type, user_id, item_id, timestamp)
+    @feedback_type = feedback_type
+    @user_id = user_id
+    @item_id = item_id
+    @timestamp = timestamp
+  end
+
+  def to_json(options = {})
+    {
+      'FeedbackType' => @feedback_type,
+      'UserId' => @user_id,
+      'ItemId' => @item_id,
+      'Timestamp' => @timestamp
+    }.to_json(options)
+  end
 end
 
 class RowAffected
+  def initialize(row_affected)
+    @row_affected = row_affected
+  end
+
+  def self.from_json(string)
+    data = JSON.load string
+    self.new data['RowAffected']
+  end
+
+  attr_reader :row_affected
 end
 
 class Gorse
@@ -10,6 +38,18 @@ class Gorse
     @api_key = api_key
   end
 
-  def insert_user
+  def insert_feedback(feedback)
+    uri = URI("#{@endpoint}/api/feedback")
+    response = Net::HTTP::post(uri, JSON.generate(feedback), {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    })
+    RowAffected.from_json(response.body)
+  end
+
+  def get_recommend(user_id)
+    uri = URI("#{@endpoint}/api/recommend/#{user_id}")
+    response = Net::HTTP::get(uri)
+    JSON.parse(response)
   end
 end
--- test/unittest.rb
+++ test/unittest.rb
@@ -1,17 +1,28 @@
+require_relative '../lib/gorse'
+
 require 'redis'
 require "test/unit"
- 
+
 class TestGorse < Test::Unit::TestCase
 
   def setup
-
+    @client = Gorse.new('http://127.0.0.1:8088')
+    @redis = Redis.new(:host => '127.0.0.1')
   end
- 
-  def test_users
 
+  def test_feedback
+    feedback = [
+      Feedback.new("read", "10", "3", "2022-11-20T13:55:27Z"),
+      Feedback.new("read", "10", "4", "2022-11-20T13:55:27Z"),
+    ]
+    row_affected = @client.insert_feedback(feedback)
+    assert_equal(2, row_affected.row_affected)
   end
 
   def test_recommend
+    @redis.zadd('offline_recommend/10', [[1, '10'], [2, '20'], [3, '30']])
+    items = @client.get_recommend('10')
+    assert_equal(%w[30 20 10], items)
   end
- 
+
 end
