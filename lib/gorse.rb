0d25a1153c85a6f4c68c879d9ca408f6790e4ffb
zhenghaoz <zhangzhenghao@hotmail.com>
2022-12-17 11:41:59 +0000

    support recommend

--- lib/gorse.rb
+++ lib/gorse.rb
@@ -1,6 +1,15 @@
+class User
+end
+
+class RowAffected
+end
+
 class Gorse
   def initialize(endpoint, api_key = "")
     @endpoint = endpoint
     @api_key = api_key
+  end
+
+  def insert_user
   end
 end
---
+++ test/unittest.rb
@@ -0,0 +1,17 @@
+require 'redis'
+require "test/unit"
+ 
+class TestGorse < Test::Unit::TestCase
+
+  def setup
+
+  end
+ 
+  def test_users
+
+  end
+
+  def test_recommend
+  end
+ 
+end
